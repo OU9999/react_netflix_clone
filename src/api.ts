2fe5fef00230f0972c7614a12cdf8bc0a913cb7f
오유진 <omh232323@gmail.com>
2023-01-20 16:02:25 +0900

    컴포넌트화 하기 전

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 const API_KEY = "c36842f25b8dc15eeb5a94ddec25b2d4";
 const BASE_PATH = "https://api.themoviedb.org/3/";
+const TAIL_PATH = `api_key=${API_KEY}&language=ko-KR&page=1&region=kr`;
 
 interface IMovies {
   id: number;
@@ -7,6 +8,9 @@ interface IMovies {
   poster_path: string;
   title: string;
   overview: string;
+  original_title: string;
+  vote_average: number;
+  release_date: string;
 }
 
 export interface IGetMoviesResult {
@@ -20,25 +24,50 @@ export interface IGetMoviesResult {
   total_results: number;
 }
 
+export interface IGenre {
+  id: number;
+  name: string;
+}
+
+export interface IDetailInfo {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+  original_title: string;
+  vote_average: number;
+  release_date: string;
+  runtime: string;
+  genres: IGenre[];
+  tagline: string;
+}
+
 const categorys = ["now_playing", "upcoming", "popular", "top_rated"];
 
 export function getMovies_nowPlaying() {
-  return fetch(
-    `${BASE_PATH}/movie/${categorys[0]}?api_key=${API_KEY}&language=ko-KR&page=1&region=kr`
-  ).then((res) => res.json());
+  return fetch(`${BASE_PATH}/movie/${categorys[0]}?${TAIL_PATH}`).then((res) =>
+    res.json()
+  );
 }
 export function getMovies_upComing() {
-  return fetch(
-    `${BASE_PATH}/movie/${categorys[1]}?api_key=${API_KEY}&language=ko-KR&page=1&region=kr`
-  ).then((res) => res.json());
+  return fetch(`${BASE_PATH}/movie/${categorys[1]}?${TAIL_PATH}`).then((res) =>
+    res.json()
+  );
 }
 export function getMovies_popular() {
-  return fetch(
-    `${BASE_PATH}/movie/${categorys[2]}?api_key=${API_KEY}&language=ko-KR&page=1&region=kr`
-  ).then((res) => res.json());
+  return fetch(`${BASE_PATH}/movie/${categorys[2]}?${TAIL_PATH}`).then((res) =>
+    res.json()
+  );
 }
 export function getMovies_topRated() {
-  return fetch(
-    `${BASE_PATH}/movie/${categorys[3]}?api_key=${API_KEY}&language=ko-KR&page=1&region=kr`
-  ).then((res) => res.json());
+  return fetch(`${BASE_PATH}/movie/${categorys[3]}?${TAIL_PATH}`).then((res) =>
+    res.json()
+  );
+}
+
+export function getDetailData(requestUrl: string, movieId: number) {
+  return fetch(`${BASE_PATH}/${requestUrl}/${movieId}?${TAIL_PATH}`).then(
+    (response) => response.json()
+  );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,10 +4,13 @@ import { useQuery } from "react-query";
 import { useMatch, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import {
+  getDetailData,
   getMovies_nowPlaying,
   getMovies_popular,
   getMovies_topRated,
   getMovies_upComing,
+  IDetailInfo,
+  IGenre,
   IGetMoviesResult,
 } from "../api";
 import { makeImagePath } from "../utils";
@@ -207,22 +210,84 @@ const BigCover = styled.div<{ bgphoto: string }>`
     url(${(props) => props.bgphoto});
 `;
 
-const BigPoster = styled.div<{ bgphoto: string }>`
-  background-image: url(${(props) => props.bgphoto});
+const BigDiv = styled.div`
+  display: flex;
+`;
+
+const BigMovieColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  top: -100px;
+  &:first-child {
+    top: -200px;
+    width: 35%;
+  }
+  &:last-child {
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 65%;
+  }
+`;
+
+const BigPoster = styled.div`
+  img {
+    border-radius: 15px;
+    width: 15vw;
+  }
 `;
 
 const BigTitle = styled.h3`
   color: ${(props) => props.theme.white.lighter};
-  padding: 20px;
   font-size: 46px;
-  position: relative;
-  top: -80px;
+  margin-bottom: 5px;
+`;
+
+const BigSubTitle = styled.h4`
+  color: ${(props) => props.theme.white.lighter};
+  font-size: 23px;
+  margin-bottom: 30px;
+`;
+
+const BigInfos = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const BigInfo = styled.span`
+  background-color: ${(props) => props.theme.black.veryDark};
+  padding: 10px;
+  border-radius: 10px;
+  margin-right: 10px;
+`;
+
+const BigVoteAverage = styled.div<{ critic: number }>`
+  width: 3vw;
+  height: 5vh;
+  border-radius: 7px;
+  text-shadow: -1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black;
+  background-color: ${(props) =>
+    props.critic > 6 ? "#66cc33" : props.critic > 4 ? "#ffc20d" : "#ff0000"};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const BigTagLine = styled.span`
+  color: ${(props) => props.theme.white.lighter};
+  font-size: 20px;
+  font-weight: bold;
+  text-shadow: -1px 0px black, 0px 1px black, 1px 0px black, 0px -1px black;
+  margin-bottom: 10px;
 `;
 
 const BigOverView = styled.p`
-  position: relative;
-  top: -80px;
-  padding: 20px;
+  width: 90%;
   color: ${(props) => props.theme.white.lighter};
 `;
 
@@ -295,6 +360,14 @@ function Home() {
     useQuery<IGetMoviesResult>(["movies", "popular"], getMovies_popular);
   const { data: data_topRated, isLoading: isLoading_topRated } =
     useQuery<IGetMoviesResult>(["movies", "topRated"], getMovies_topRated);
+  const { data: data_detail } = useQuery<IDetailInfo>(
+    [
+      "nowPlaying" + bigMovieMatch?.params.movieId,
+      "detail" + bigMovieMatch?.params.movieId,
+    ],
+    () =>
+      getDetailData("movie", bigMovieMatch?.params.movieId as unknown as number)
+  );
 
   const [index, setIndex] = useState(0);
   const [leaving, setLeaving] = useState(false);
@@ -322,11 +395,25 @@ function Home() {
     navigation(`/movies/${movieId}`);
   };
   const onOverlayClicked = () => navigation(-1);
+
   const clickedMovie =
     bigMovieMatch?.params.movieId &&
     data_nowPlaying?.results.find(
       (movie) => movie.id + "" === bigMovieMatch.params.movieId
     );
+
+  const getGenreToString = (arr: IGenre[]): string => {
+    if (arr && arr.length > 0) {
+      return (
+        arr.map((g, idx) => {
+          return idx + 1 === arr.length ? `${g.name}` : `${g.name}`;
+        }) + ""
+      );
+    }
+    return "";
+  };
+
+  console.log(data_detail?.genres);
 
   return (
     <Wrapper>
@@ -355,7 +442,13 @@ function Home() {
               </Button>
               <Button>
                 <FontAwesomeIcon icon={faCircleInfo} />
-                <span>상세 정보</span>
+                <span
+                  onClick={() =>
+                    onBoxClicked(data_nowPlaying?.results[0].id as number)
+                  }
+                >
+                  상세 정보
+                </span>
               </Button>
             </Buttons>
           </Banner>
@@ -428,7 +521,6 @@ function Home() {
                 </Row>
               </AnimatePresence>
             </Slider>
-            {/* upComing */}
           </Sliders>
 
           <AnimatePresence>
@@ -443,11 +535,39 @@ function Home() {
                   {clickedMovie && (
                     <>
                       <BigCover
-                        bgphoto={makeImagePath(clickedMovie.backdrop_path)}
+                        bgphoto={makeImagePath(
+                          clickedMovie.backdrop_path || clickedMovie.poster_path
+                        )}
                       />
-                      <BigPoster />
-                      <BigTitle>{clickedMovie.title}</BigTitle>
-                      <BigOverView>{clickedMovie.overview}</BigOverView>
+                      <BigDiv>
+                        <BigMovieColumn>
+                          <BigPoster>
+                            <img
+                              src={makeImagePath(clickedMovie.poster_path)}
+                            />
+                          </BigPoster>
+                        </BigMovieColumn>
+                        <BigMovieColumn>
+                          <BigTitle>{clickedMovie.title}</BigTitle>
+                          <BigSubTitle>
+                            {clickedMovie.original_title}
+                          </BigSubTitle>
+                          <BigInfos>
+                            <BigInfo>
+                              {clickedMovie.release_date.slice(0, 4)}
+                            </BigInfo>
+                            <BigInfo>{`${data_detail?.runtime}분`}</BigInfo>
+                            <BigInfo>
+                              {getGenreToString(data_detail?.genres || [])}
+                            </BigInfo>
+                            <BigVoteAverage critic={clickedMovie.vote_average}>
+                              {clickedMovie.vote_average * 10}
+                            </BigVoteAverage>
+                          </BigInfos>
+                          <BigTagLine>{data_detail?.tagline}</BigTagLine>
+                          <BigOverView>{clickedMovie.overview}</BigOverView>
+                        </BigMovieColumn>
+                      </BigDiv>
                     </>
                   )}
                 </BigMovie>
