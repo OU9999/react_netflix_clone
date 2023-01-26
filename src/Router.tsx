34006e7d3a75a0b1070e7ef6d14d5d92db5adfcd
오유진 <omh232323@gmail.com>
2023-01-26 15:57:04 +0900

    라우터 basename 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,40 +1,48 @@
+import { basename } from "path";
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import Home from "./screens/Home";
 import Search from "./screens/Search";
 import Tv from "./screens/Tv";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: `/`,
+      element: <Root />,
+
+      children: [
+        {
+          path: "",
+          element: <Home />,
+          index: true,
+        },
+        {
+          path: "movies/:movieId",
+          element: <Home />,
+        },
+        {
+          path: "tv",
+          element: <Tv />,
+        },
+        {
+          path: "tvs/:tvId",
+          element: <Tv />,
+        },
+        {
+          path: "search",
+          element: <Search />,
+        },
+        {
+          path: "/search/:menuName/:searchId",
+          element: <Search />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        path: "movies/:movieId",
-        element: <Home />,
-      },
-      {
-        path: "tv",
-        element: <Tv />,
-      },
-      {
-        path: "tvs/:tvId",
-        element: <Tv />,
-      },
-      {
-        path: "search",
-        element: <Search />,
-      },
-      {
-        path: "/search/:menuName/:searchId",
-        element: <Search />,
-      },
-    ],
-  },
-]);
+    basename: process.env.PUBLIC_URL,
+  }
+);
 
 export default router;
